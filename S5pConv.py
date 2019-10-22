0c6fe2e20c116dae4de6e65e43213c773d17dea3
Yukikazari <haya_nanakusa793@yahoo.co.jp>
2019-10-22 23:58:47 +0900

    更新

--- S5pConv.py
+++ S5pConv.py
@@ -7,80 +7,76 @@ import wx
 
 #https://github.com/Yukikazari/V-Conv.git
 
-"""def vprconv(infile, outfile):
-    try:
-        with open('./dic/Phonetic.json', encoding='utf-8') as f:#発音記号変換用
-            pho = json.load(f)
-    except FileNotFoundError:
-        print('発音辞書がありません')
-    try:
-        with open('./dic/uPhonetic.json', encoding='utf-8') as f:#発音記号変換用
-            upho = json.load(f)
-    except FileNotFoundError:
-        with open('./dic/uPhonetic.json', 'w') as f:
-            pass
-        upho = {}
+class VprConv():
+    def __init__(self, infile, outfile):
+        self.infile = infile
+        self.outfile = outfile
+        self.Conv()
 
-    pho.update(upho)
+    def Conv(self):
 
-    with open(infile, encoding='utf-8') as f:#s5p読み込み
-        s5pj = json.load(f)
 
-    vprj = {"title": "null", "masterTrack": {"samplingRate": 44100, "tempo": {"global": {"isEnabled": False, "value": 12000}, "events": []}, "timeSig": {"events": []}, "volume": {"events": [{"pos": 0, "value": 0}]}}, "voices": [{"compID": "AKR", "name": "Mishima_Furikake"}], "tracks": []}
 
-    #トラックテンプレート
-    t_temp = {"name": "akari", "volume": {"events": [{"pos": 0, "value": 0}]}, "parts": [{"pos": 30720, "duration": 168480, "styleName": "VOICEROID2 Akari", "voice": {"compID": "10980+Tax", "langID": 0}, "notes": []}]}
 
-    name = os.path.splitext("north.s5p")[0]
+        vprj = {"title": "null", "masterTrack": {"samplingRate": 44100, "tempo": {"global": {"isEnabled": False, "value": 12000}, "events": []}, "timeSig": {"events": []}, "volume": {"events": [{"pos": 0, "value": 0}]}}, "voices": [{"compID": "AKR", "name": "Mishima_Furikake"}], "tracks": []}
 
-    vprj["title"] = name #タイトル
-    vprj["masterTrack"]["tempo"]["global"]["value"] = s5pj["tempo"][0]["beatPerMinute"] * 100
-    #最初のとこ
+        #トラックテンプレート
+        t_temp = {"name": "akari", "volume": {"events": [{"pos": 0, "value": 0}]}, "parts": [{"pos": 30720, "duration": 168480, "styleName": "VOICEROID2 Akari", "voice": {"compID": "10980+Tax", "langID": 0}, "notes": []}]}
 
-    for i in range(len(s5pj["tempo"])):#テンポ
-        tmp = {}
-        tmp["pos"] = s5pj["tempo"][i]["position"]
-        tmp["value"] = s5pj["tempo"][i]["beatPerMinute"] * 100
+        name = os.path.splitext("north.s5p")[0]
 
-        vprj["masterTrack"]["tempo"]["events"].append(tmp)
+        vprj["title"] = name #タイトル
+        vprj["masterTrack"]["tempo"]["global"]["value"] = s5pj["tempo"][0]["beatPerMinute"] * 100
+        #最初のとこ
 
-    for i in range(len(s5pj["meter"])):  #拍子
-        tmp = {}
-        tmp["bar"] = s5pj["meter"][i]["measure"]
-        tmp["numer"] = s5pj["meter"][i]["beatPerMeasure"]
-        tmp["denom"] = s5pj["meter"][i]["beatGranularity"]
-        vprj["masterTrack"]["timeSig"]["events"].append(tmp)
+        for i in range(len(s5pj["tempo"])):#テンポ
+            tmp = {}
+            tmp["pos"] = s5pj["tempo"][i]["position"]
+            tmp["value"] = s5pj["tempo"][i]["beatPerMinute"] * 100
 
-    for j in range(len(s5pj["tracks"])):
-        vprj["tracks"].append(t_temp)
-        st_sy = int(s5pj["tracks"][j]["notes"][0]["onset"])
-        st =  int(st_sy / 2822400000)  #開始ポイント
-        vprj["tracks"][j]["parts"][0]["pos"] = st * 1920 #1分57600???
+            vprj["masterTrack"]["tempo"]["events"].append(tmp)
 
-        pos = int(st * 2822400000)#s5pの開始タイム
+        for i in range(len(s5pj["meter"])):  #拍子
+            tmp = {}
+            tmp["bar"] = s5pj["meter"][i]["measure"]
+            tmp["numer"] = s5pj["meter"][i]["beatPerMeasure"]
+            tmp["denom"] = s5pj["meter"][i]["beatGranularity"]
+            vprj["masterTrack"]["timeSig"]["events"].append(tmp)
 
-        for i in range(len(s5pj["tracks"][j]["notes"])):#歌詞
-            tmp = {"lyric": "", "phoneme": "", "pos": 0, "duration": 0, "number": 0, "velocity": 64}
-            lyric = s5pj["tracks"][j]["notes"][i]["lyric"]
-            tmp["lyric"] = lyric
-            tmp["phoneme"] = pho[lyric]
-            tmp["pos"] = int((s5pj["tracks"][j]["notes"][i]["onset"] - pos) / 1470000 )#空白忘れてた
-            tmp["duration"] = int(s5pj["tracks"][j]["notes"][i]["duration"] / 1470000)
-            tmp["number"] = int(s5pj["tracks"][j]["notes"][i]["pitch"])
-            dur = int(tmp["pos"] + tmp["duration"])
-            vprj["tracks"][j]["parts"][0]["notes"].append(tmp) 
+        for j in range(len(s5pj["tracks"])):
+            vprj["tracks"].append(t_temp)
+            st_sy = int(s5pj["tracks"][j]["notes"][0]["onset"])
+            st =  int(st_sy / 2822400000)  #開始ポイント
+            vprj["tracks"][j]["parts"][0]["pos"] = st * 1920 #1分57600???
 
-        vprj["tracks"][j]["parts"][0]["duration"] =  dur
+            pos = int(st * 2822400000)#s5pの開始タイム
 
-    dir_name = './Project'
-    os.makedirs("./Project/Project", exist_ok=True)
+            for i in range(len(s5pj["tracks"][j]["notes"])):#歌詞
+                tmp = {"lyric": "", "phoneme": "", "pos": 0, "duration": 0, "number": 0, "velocity": 64}
+                lyric = s5pj["tracks"][j]["notes"][i]["lyric"]
+                tmp["lyric"] = lyric
+                tmp["phoneme"] = pho[lyric]
+                tmp["pos"] = int((s5pj["tracks"][j]["notes"][i]["onset"] - pos) / 1470000 )#空白忘れてた
+                tmp["duration"] = int(s5pj["tracks"][j]["notes"][i]["duration"] / 1470000)
+                tmp["number"] = int(s5pj["tracks"][j]["notes"][i]["pitch"])
+                dur = int(tmp["pos"] + tmp["duration"])
+                vprj["tracks"][j]["parts"][0]["notes"].append(tmp) 
 
-    with codecs.open('./Project/Project/sequence.json', 'w', 'utf-8') as f:
-        json.dump(vprj, f, ensure_ascii=False)
+            vprj["tracks"][j]["parts"][0]["duration"] =  dur
 
-    shutil.make_archive(dir_name, 'zip', root_dir=dir_name)
-    os.rename(dir_name + '.zip', outfile + '.vpr')
-    shutil.rmtree("./Project/Project")"""
+        dir_name = './Project'
+        os.makedirs("./Project/Project", exist_ok=True)
+
+        with codecs.open('./Project/Project/sequence.json', 'w', 'utf-8') as f:
+            json.dump(vprj, f, ensure_ascii=False)
+
+        shutil.make_archive(dir_name, 'zip', root_dir=dir_name)
+        try:
+            os.rename(dir_name + '.zip', self.outfile)
+        except WindowsError:
+            os.remove(self.outfile)
+            os.rename(dir_name + '.zip', self.outfile)
+        shutil.rmtree("./Project/Project")
 
 class FileDrop(wx.FileDropTarget):
     def __init__(self, window):
@@ -91,84 +87,137 @@ class FileDrop(wx.FileDropTarget):
         infile = files[len(files) - 1]
         if os.path.splitext(infile)[1] == '.s5p':
             self.window.infile = infile
-            self.window.text21.SetLabel(infile)
-            self.window.text22.SetLabel(os.path.splitext(infile)[0] + '.vpr')
-
-
+            self.window.outvpr = os.path.splitext(infile)[0] + '.vpr'
+            self.window.infile_t.SetLabel(infile)
+            self.window.outvpr_t.SetLabel(os.path.splitext(infile)[0] + '.vpr')
         return 0
 
-class SelectFiles():
-    def __init__(self, window, infile):
+class FileSelect():
+    def __init__(self, window, SetId):
+        self.ope = {0: ".s5p", 1: ".vpr"}
         self.window = window
+        if SetId == 202:
+            self.S5p()
+        elif SetId == 212:
+            self.Vpr()
+        else:
+            pass
 
-    def OnSelectFiles(self, window, infile):
-        ope = {0: ".s5p", 1: ".vpr"}
-        first_path = os.path.splitext(infile)[0] + ope[0]
-        dialog = wx.FileDialog(ope[0], u'ファイルを選択してください', first_path)  #atode
+    def S5p(self):
+        first_path = os.path.dirname(self.window.infile)
+        first_file = os.path.basename(self.window.infile)
+        filter = "SynthV file(*.s5p) | *.s5p"
+        dialog = wx.FileDialog(None, '開く', first_path, first_file, filter, style=wx.FD_OPEN)  #atode
         dialog.ShowModal()
+        if not dialog.GetPath() == '':
+            infile = dialog.GetPath()
+            self.window.infile = infile
+            self.window.outvpr = os.path.splitext(infile)[0] + '.vpr'
+            self.window.infile_t.SetLabel(infile)
+            self.window.outvpr_t.SetLabel(os.path.splitext(infile)[0] + '.vpr')
 
-        return 0
-
+    def Vpr(self):
+        first_path = os.path.dirname(self.window.outvpr)
+        first_file = os.path.basename(self.window.outvpr)
+        filter = "VOCALOID5 file(*.vpr) | *.vpr"
+        dialog = wx.FileDialog(None, '保存', first_path, first_file, filter, style=wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)  #atode
+        dialog.ShowModal()
+        if not dialog.GetPath() == '':
+            outfile = dialog.GetPath()
+            self.window.outvpr = outfile
+            self.window.outvpr_t.SetLabel(outfile)
 
 class MainFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, wx.ID_ANY, "V-Conv", size=(650, 300))
+
+
+        wx.Frame.__init__(self, None, wx.ID_ANY, "V-Conv", size=(700, 300))
 
         panel = wx.Panel(self, wx.ID_ANY)
         hbox = wx.BoxSizer(wx.HORIZONTAL)
         font_e = wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False, "Segoe UI")
         font_j = wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False, "メイリオ")
 
-        infile = ''
-        out_vpr = ''
+        self.infile = ''
+        self.outvpr = ''
 
         self.SetDropTarget(FileDrop(self))
 
         #Panel 1
         panel1 = wx.Panel(panel, wx.ID_ANY)
-        box1 = wx.BoxSizer(wx.VERTICAL)
-        
-        text11 = wx.CheckBox(panel1, wx.ID_ANY, 'vpr (VOCALOID5)')
-        text11.SetFont(font_e)        
-        text12 = wx.CheckBox(panel1, wx.ID_ANY, 'ust (UTAU)')
-        text12.SetFont(font_e)
 
-        box1.Add(wx.StaticText(panel1, wx.ID_ANY, ""), 1)
-        box1.Add(text11, 1)
-        box1.Add(text12, 1)
+        box1_name = wx.StaticBox(panel1, wx.ID_ANY, '出力ファイル形式')
+        box1 = wx.StaticBoxSizer(box1_name, wx.VERTICAL)
+        
+        self.vpr_c = wx.CheckBox(panel1, 101, 'vpr (VOCALOID5)')
+        self.vpr_c.SetFont(font_e)
+        self.vpr_c.SetValue(True)
+        self.vpr_c.Disable()
+        
+        self.ust_c = wx.CheckBox(panel1, 102, 'ust (UTAU)')
+        self.ust_c.SetFont(font_e)
+        self.ust_c.Disable()  #ここ
+
+        box1.Add(self.vpr_c, 1, wx.TOP, 10)
+        box1.Add(self.ust_c, 1,)
+        box1.Add(wx.StaticText(panel1, wx.ID_ANY, ""))
         panel1.SetSizer(box1)
-        hbox.Add(panel1, 0, wx.TOP | wx.LEFT, 10)
+        hbox.Add(panel1, 0, wx.TOP | wx.LEFT | wx.RIGHT, 10)
 
         #Panel 2
         panel2 = wx.Panel(panel, -1)
         box2 = wx.BoxSizer(wx.VERTICAL)
 
-        self.text21 = wx.TextCtrl(panel2, wx.ID_ANY, infile)
-        self.btn21 = wx.Button(panel2, wx.ID_ANY, "…", size=(30, -1))
-        self.Bind(wx.EVT_BUTTON, SelectFiles(self, infile), self.btn21)
+        self.infile_t = wx.TextCtrl(panel2, 201, self.infile)
+        infile_b = wx.Button(panel2, 202, "…", size=(30, -1))
+        infile_b.Bind(wx.EVT_BUTTON, self.OnSelectFiles)
 
-        grid21 = wx.FlexGridSizer(rows=6, cols=2, gap=(0, 0))
-        grid21.Add(wx.StaticText(panel2, wx.ID_ANY, "    変換元ファイル"), wx.ID_ANY, wx.EXPAND)
-        grid21.Add(wx.StaticText(panel2, wx.ID_ANY, ""))
-        grid21.Add(self.text21, wx.ID_ANY, wx.EXPAND)
-        grid21.Add(self.btn21, wx.ID_ANY, wx.ALIGN_RIGHT)
-        grid21.Add(wx.StaticText(panel2, wx.ID_ANY, ""), wx.ID_ANY, wx.EXPAND)
-        grid21.Add(wx.StaticText(panel2, wx.ID_ANY, ""))
+        self.outvpr_t = wx.TextCtrl(panel2, 211, self.outvpr)
+        outvpr_b = wx.Button(panel2, 212, "…", size=(30, -1))
+        outvpr_b.Bind(wx.EVT_BUTTON, self.OnSelectFiles)
+
+        self.outust_t = wx.TextCtrl(panel2, 221, "工事中")
+        outust_b = wx.Button(panel2, 222, "…", size=(30, -1))
+        outust_b.Bind(wx.EVT_BUTTON, self.OnSelectFiles)
+        self.outust_t.Disable()  #ここ
+        outust_b.Disable()  #ここ
+
+
+        box21_name = wx.StaticBox(panel2, wx.ID_ANY, '変換元ファイル')
+        box21 = wx.StaticBoxSizer(box21_name, wx.VERTICAL)
+
+        grid21 = wx.FlexGridSizer(rows=1, cols=2, gap=(0, 0))
+        grid21.Add(self.infile_t, wx.ID_ANY, wx.EXPAND | wx.TOP | wx.BOTTOM, 5)
+        grid21.Add(infile_b, wx.ID_ANY, wx.ALIGN_RIGHT | wx.TOP | wx.BOTTOM, 5)
         grid21.AddGrowableCol(0)
-        box2.Add(grid21, 1, wx.EXPAND)
 
-        grid22 = wx.FlexGridSizer(rows=6, cols=2, gap=(0, 0))
-        grid22.Add(wx.StaticText(panel2, wx.ID_ANY, "    変換先ファイル"), wx.ID_ANY, wx.EXPAND)
+        box21.Add(grid21, 1, wx.EXPAND | wx.BOTTOM, 7)
+        box2.Add(box21, 0, wx.EXPAND)
+
+        box22_name = wx.StaticBox(panel2, wx.ID_ANY, '変換先ファイル')
+        box22 = wx.StaticBoxSizer(box22_name, wx.VERTICAL)
+
+        grid22 = wx.FlexGridSizer(rows=2, cols=2, gap=(0, 0))
+        grid22.Add(wx.StaticText(panel2, wx.ID_ANY, "    vprファイル"))
         grid22.Add(wx.StaticText(panel2, wx.ID_ANY, ""))
-        grid22.Add(wx.TextCtrl(panel2, wx.ID_ANY, "C:/"), wx.ID_ANY, wx.EXPAND)
-        grid22.Add(wx.Button(panel2, wx.ID_ANY, "…", size=(30, -1)), wx.ID_ANY, wx.ALIGN_RIGHT)
-        grid22.Add(wx.StaticText(panel2, wx.ID_ANY, ""))
-        grid22.Add(wx.StaticText(panel2, wx.ID_ANY, ""))
+        grid22.Add(self.outvpr_t, wx.ID_ANY, wx.EXPAND)
+        grid22.Add(outvpr_b, wx.ID_ANY, wx.ALIGN_RIGHT)
         grid22.AddGrowableCol(0)
-        box2.Add(grid22, 1, wx.EXPAND | wx.TOP, 5)
+        box22.Add(grid22, 1, wx.EXPAND | wx.BOTTOM, 10)
+
+        grid23 = wx.FlexGridSizer(rows=2, cols=2, gap=(0, 0))
+        grid23.Add(wx.StaticText(panel2, wx.ID_ANY, "    ustファイル"))
+        grid23.Add(wx.StaticText(panel2, wx.ID_ANY, ""))
+        grid23.Add(self.outust_t, wx.ID_ANY, wx.EXPAND)
+        grid23.Add(outust_b, wx.ID_ANY, wx.ALIGN_RIGHT)
+        grid23.AddGrowableCol(0)
+        box22.Add(grid23, 1, wx.EXPAND | wx.BOTTOM, 10)
+
+
+        box2.Add(box22, 1, wx.EXPAND)
 
         panel2.SetSizer(box2)
-        hbox.Add(panel2, 1, wx.TOP, 15)
+        hbox.Add(panel2, 1, wx.TOP, 10)
 
         #Panel 3
         panel3 = wx.Panel(panel, -1)
@@ -177,14 +226,21 @@ class MainFrame(wx.Frame):
         box32 = wx.BoxSizer(wx.VERTICAL)
 
         font3 = wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False, "メイリオ")
+
         text31 = wx.Button(panel3, wx.ID_ANY, "変換", size=(120, 60))
         text31.SetFont(font3)
+        text31.Bind(wx.EVT_BUTTON, self.OnConversion)
+
         text32 = wx.Button(panel3, wx.ID_ANY, "ノート編集", size=(120, -1))
         text32.SetFont(font_j)
+
         text33 = wx.Button(panel3, wx.ID_ANY, "使い方", size=(100, -1))
         text33.SetFont(font_j)
+
         text34 = wx.Button(panel3, wx.ID_ANY, "設定", size=(100, -1))
         text34.SetFont(font_j)
+        text34.Bind(wx.EVT_BUTTON, self.OnSettings)
+
 
         box31.Add(wx.StaticText(panel3, wx.ID_ANY, ""))
         box31.Add(text31, flag=wx.CENTER | wx.ALIGN_TOP)
@@ -197,31 +253,132 @@ class MainFrame(wx.Frame):
         grid3.Add(box32, 0, wx.ALIGN_BOTTOM | wx.CENTER |wx.LEFT | wx.BOTTOM, 10)
         
         panel3.SetSizer(grid3)
-        hbox.Add(panel3, 0, wx.TOP | wx.RIGHT | wx.LEFT | wx.EXPAND, 10)
+        hbox.Add(panel3, 0,wx.RIGHT | wx.LEFT | wx.EXPAND, 10)
 
         panel.SetSizer(hbox)
+
+        self.Centre()
+
+    def ReadSettings(self):
+        self.base = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(self.base)
+        try:
+            with open('./dic/Phonetic.json', encoding='utf-8') as f:#発音記号変換用
+                self.pho = json.load(f)
+        except FileNotFoundError:
+            print('発音辞書がありません')
+        try:
+            with open('./dic/uPhonetic.json', encoding='utf-8') as f:#発音記号変換用
+                upho = json.load(f)
+        except FileNotFoundError:
+            with open('./dic/uPhonetic.json', 'w') as f:
+                pass
+            upho = {}
+
+        self.pho.update(upho)
+
+    def ReadS5p(self):
+        with open(self.infile, encoding='utf-8') as f:#s5p読み込み
+            s5pj = json.load(f)
+
+        self.s5pf = {"notes": []}
+        
+        for j in range(len(s5pj["tracks"])):
+            st_sy = int(s5pj["tracks"][j]["notes"][0]["onset"])
+            st =  int(st_sy / 2822400000)  #開始ポイント
+            vprj["tracks"][j]["parts"][0]["pos"] = st * 1920 #1分57600???
+
+            pos = int(st * 2822400000)#s5pの開始タイム
+
+            for i in range(len(s5pj["tracks"][j]["notes"])):#歌詞
+                tmp = {"lyric": "", "phoneme": "", "pos": 0, "duration": 0, "number": 0, "velocity": 64}
+                lyric = s5pj["tracks"][j]["notes"][i]["lyric"]
+                tmp["lyric"] = lyric
+                tmp["phoneme"] = pho[lyric]
+                tmp["pos"] = int((s5pj["tracks"][j]["notes"][i]["onset"] - pos) / 1470000 )#空白忘れてた
+                tmp["duration"] = int(s5pj["tracks"][j]["notes"][i]["duration"] / 1470000)
+                tmp["number"] = int(s5pj["tracks"][j]["notes"][i]["pitch"])
+                dur = int(tmp["pos"] + tmp["duration"])
+                vprj["tracks"][j]["parts"][0]["notes"].append(tmp) 
+
+            vprj["tracks"][j]["parts"][0]["duration"] =  dur
+
+
+    def OnSelectFiles(self, event):
+        SetId = event.GetId()
+        FileSelect(self, SetId)
+        return 0
+
+    def OnConversion(self, event):
+        self.infile = self.infile_t
+        self.outvpr = self.outvpr_t
+
+        if self.vpr_c.GetValue() == True:
+            VprConv(self.infile, self.outvpr)
+        elif self.ust_c.GetValue() == True:
+            pass
+
+    def OnSettings(self, event):
+        SetFrame().Show()
 
 
 class SetFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, wx.ID_ANY, "V-Conv", size=(650, 300))
+        wx.Frame.__init__(self, None, wx.ID_ANY, "設定", size=(400, 300))
+        notebook = wx.Notebook(self, wx.ID_ANY)
 
-        panel = wx.Panel(self, wx.ID_ANY)
-        hbox = wx.BoxSizer(wx.VERTICAL)
+        panel1 = wx.Panel(notebook, wx.ID_ANY)
+        panel2 = wx.Panel(notebook, wx.ID_ANY)
 
-        hbox.Add(wx.Button(panel, wx.ID_ANY, "…", size=(30, 30)), 0, wx.ALIGN_BOTTOM)
-        hbox.Add(wx.Button(panel, wx.ID_ANY, "…", size=(30, 30)), 0, wx.ALIGN_BOTTOM)
-        panel.SetSizer(hbox)
+        notebook.InsertPage(0, panel1, '保存設定')
+        notebook.InsertPage(1, panel2, 'ノート編集')
+
+        box1 = wx.BoxSizer(wx.VERTICAL)
+        cb11 = wx.CheckBox(panel1, wx.ID_ANY, '保存先フォルダを固定する')
+
+        box1.Add(cb11)
+
+        panel1.SetSizer(box1)
+
+        box2 = wx.BoxSizer(wx.VERTICAL)
+        cb21 = wx.CheckBox(panel2, wx.ID_ANY, '文字種による無声化を有効にする')
+        cb22 = wx.CheckBox(panel2, wx.ID_ANY, 'ベロシティの変更を有効にする')
+
+        box2.Add(cb21)
+        box2.Add(cb22)
+
+        panel2.SetSizer(box2)
+
+        self.Centre()
 
 class NoteFrame(wx.Frame):
     def __init__(self):
-        pass
+        wx.Frame.__init__(self, None, wx.ID_ANY, "ノート編集", size=(400, 300))
+
+        panel = wx.Panel(self, wx.ID_ANY)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        font_e = wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False, "Segoe UI")
+        font_j = wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False, "メイリオ")
+
+
+        for i in range(5):
+            box[i] = wx.BoxSizer(wx.HORIZONTAL)
+            lyric[i]
+            phoneme[i]
+            duration[i]
+            number[i]
+            velocity[i]
+
+        panel.SetSizer(vbox, 1, wx.ALL, 20)
+
+
 
 
 if __name__ == "__main__":
 
     app = wx.App()
-    MainFrame().Show()
+    #MainFrame().Show()
+    NoteFrame().Show()
     app.MainLoop()
 
 
